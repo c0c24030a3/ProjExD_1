b42f4b54caa4bcdd97ae97d4b2946f70df96915b
M_Sato <c0c24030a3@edu.teu.ac.jp>
2025-06-10 16:42:50 +0900

    課題2

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -24,6 +24,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
+        kk_rct.move_ip(-1,0)#1.①
+
         key_lst=pg.key.get_pressed()#押されたキー取得
         if key_lst[pg.K_UP]:         
             kk_rct.move_ip((0,-1))
@@ -32,7 +34,7 @@ def main():
         if key_lst[pg.K_LEFT]:         
             kk_rct.move_ip((-1,0))
         if key_lst[pg.K_RIGHT]:         
-            kk_rct.move_ip((1,0))
+            kk_rct.move_ip((2,0))
         
         screen.blit(bg_img, [-tmr, 0])
         screen.blit(bg_img2,[-tmr+1600,0])
